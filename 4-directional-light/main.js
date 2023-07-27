06470ec42b13388577d89620dd1649701efa813c
Victoria Cheng <victoria.cheng15@gmail.com>
2023-07-27 05:49:13 -0600

    chore: add ability to adjust light colors and ambient light

--- 4-directional-light/main.js
+++ 4-directional-light/main.js
@@ -23,10 +23,24 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(3, 4, 5);
 scene.add(camera);
 
-const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.5);
+const lightColors = {
+	directional: "#00fffc",
+	ambient: "#404040",
+};
+
+const directionalLight = new THREE.DirectionalLight(
+	lightColors.directional,
+	0.5,
+);
 directionalLight.position.set(1, 1, 1);
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.5);
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+	directionalLight,
+	0.5,
+);
 scene.add(directionalLight, directionalLightHelper);
+
+const ambientLight = new THREE.AmbientLight(lightColors.ambient, 0.5);
+scene.add(ambientLight);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
@@ -34,55 +48,86 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const controls = new OrbitControls(camera, canvas);
 
-const material = new THREE.MeshStandardMaterial({ roughness: 0.5, metalness: 0 });
+const material = new THREE.MeshStandardMaterial({
+	roughness: 0.5,
+	metalness: 0,
+});
 const materialFolder = gui.addFolder("Material");
-materialFolder.add(material, "roughness").min(0).max(1).step(0.001).onChange((value) => {
-	material.roughness = value
-})
-materialFolder.add(material, "metalness").min(0).max(1).step(0.001).onChange((value) => {
-	material.metalness = value
-})
+materialFolder.add(material, "roughness", 0, 1, 0.001).onChange((value) => {
+	material.roughness = value;
+});
+materialFolder.add(material, "metalness", 0, 1, 0.001).onChange((value) => {
+	material.metalness = value;
+});
 
-const directionalFolder = gui.addFolder("Directional Light")
-directionalFolder.add(directionalLight, 'visible').name("Visible")
-directionalFolder.add(directionalLightHelper, 'visible').name("Helper Visible")
-directionalFolder.add(directionalLight, 'intensity').min(1).max(4).step(0.001).name("Intensity")
-directionalFolder.add(directionalLight.position, "x").min(-5).max(5).step(0.001).name("X")
-directionalFolder.add(directionalLight.position, "y").min(-5).max(5).step(0.001).name("Y")
-directionalFolder.add(directionalLight.position, "z").min(-5).max(5).step(0.001).name("Z")
+/* 
+directional and ambient light GUI
+*/
+const directionalFolder = gui.addFolder("Directional Light");
+directionalFolder.add(directionalLight, "visible").name("Visible");
+directionalFolder.add(directionalLightHelper, "visible").name("Helper Visible");
+directionalFolder
+	.add(directionalLight, "intensity")
+	.min(0)
+	.max(2)
+	.step(0.001)
+	.name("Intensity");
+directionalFolder.addColor(lightColors, "directional").onChange(() => {
+	directionalLight.color.set(lightColors.directional);
+});
+directionalFolder
+	.add(directionalLight.position, "x")
+	.min(-5)
+	.max(5)
+	.step(0.001)
+	.name("X");
+directionalFolder
+	.add(directionalLight.position, "y")
+	.min(-5)
+	.max(5)
+	.step(0.001)
+	.name("Y");
+directionalFolder
+	.add(directionalLight.position, "z")
+	.min(-5)
+	.max(5)
+	.step(0.001)
+	.name("Z");
 
-const sphere = new THREE.Mesh(
-	new THREE.SphereGeometry(0.5, 32, 32),
-	material
-)
-sphere.position.x = - 2
+const ambientFolder = gui.addFolder("Ambient Light");
+ambientFolder.add(ambientLight, "visible").name("Visible");
+ambientFolder
+	.add(ambientLight, "intensity")
+	.min(0)
+	.max(1)
+	.step(0.001)
+	.name("Intensity");
+ambientFolder.addColor(lightColors, "ambient").onChange(() => {
+	ambientLight.color.set(lightColors.ambient);
+});
 
-const cube = new THREE.Mesh(
-	new THREE.BoxGeometry(0.75, 0.75, 0.75),
-	material
-)
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.position.x = -2;
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
 
 const torus = new THREE.Mesh(
 	new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-	material
-)
-torus.position.x = 2
+	material,
+);
+torus.position.x = 2;
 
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(15, 15), material);
+plane.rotation.x = -Math.PI * 0.5;
+plane.position.y = -0.65;
 
-const plane = new THREE.Mesh(
-	new THREE.PlaneGeometry(15, 15),
-	material
-)
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.65
-
-scene.add(plane)
+scene.add(plane);
 
 const shapes = [sphere, cube, torus];
 
-shapes.forEach((shape) => {
-	scene.add(shape);
-})
+for (const shape of shapes) {
+	scene.add(shape)
+}
 
 const clock = new THREE.Clock();
 
